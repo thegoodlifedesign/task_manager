6e6a92b63e5d1d08fcf821c67cb51e567cbfffaf
rodrigo <lol@lol.lol>
2014-12-14 01:08:25 -0500

    test for live site

--- resources/templates/task/task-details.blade.php
+++ resources/templates/task/task-details.blade.php
@@ -67,6 +67,8 @@
                             <input type="hidden" name="task_id" value="{{$task->id}}">
                             <input class="btn btn-primary" type="submit" value="accept">
                         {!! Form::close() !!}
+                    @else
+                        sup dude
                     @endif
                     @if(App::make('taskHelpers')->userAcceptedTask($task))
                         {!! Form::open(['url' => URL::route('start.task')]) !!}
