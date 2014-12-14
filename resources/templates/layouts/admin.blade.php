f1933c0d0a9a61d1d6486c531cd3e77d0f5cf2d4
rodrigo <lol@lol.lol>
2014-12-14 13:20:14 -0500

    css fixes

--- resources/templates/layouts/admin.blade.php
+++ resources/templates/layouts/admin.blade.php
@@ -3,17 +3,24 @@
 @section('content')
 
 <div id="adminWrapper">
-    <div id="sideBar">
-        <ul class="nav nav-sidebar">
-            <li><a href="{!! URL::route('projects') !!}">Projects</a></li>
-            <li><a href="{!! URL::route('add.task') !!}">Add Task</a></li>
-            <li><a href="{!! URL::route('completed.tasks', $attributes = ['username' => Auth::user()->username]) !!}">Completed Tasks</a></li>
-            <li><a href="{!! URL::route('started.tasks', $attributes = ['username' => Auth::user()->username]) !!}">Started Tasks</a></li>
-            <li><a href="{!! URL::route('accepted.tasks', $attributes = ['username' => Auth::user()->username]) !!}">Accepted Tasks</a></li>
-            <li><a href="{!! URL::route('assigned.tasks', $attributes = ['username' => Auth::user()->username]) !!}">Assigned Tasks</a></li>        </ul>
-    </div>
-    <div id="mainBody">
-        @yield('adminContent')
+    <div class="row">
+        <div class="col-md-2">
+            <div id="sideBar">
+                <ul class="nav nav-sidebar">
+                    <li><a href="{!! URL::route('projects') !!}">Projects</a></li>
+                    <li><a href="{!! URL::route('add.task') !!}">Add Task</a></li>
+                    <li><a href="{!! URL::route('completed.tasks', $attributes = ['username' => Auth::user()->username]) !!}">Completed Tasks</a></li>
+                    <li><a href="{!! URL::route('started.tasks', $attributes = ['username' => Auth::user()->username]) !!}">Started Tasks</a></li>
+                    <li><a href="{!! URL::route('accepted.tasks', $attributes = ['username' => Auth::user()->username]) !!}">Accepted Tasks</a></li>
+                    <li><a href="{!! URL::route('assigned.tasks', $attributes = ['username' => Auth::user()->username]) !!}">Assigned Tasks</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-10">
+            <div id="mainBody">
+                @yield('adminContent')
+            </div>
+        </div>
     </div>
 </div>
 
