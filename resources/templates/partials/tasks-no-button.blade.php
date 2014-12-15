19d5cf94e4aae6a517931368432615ba6b6ab7b9
rodrigo <lol@lol.lol>
2014-12-14 23:18:09 -0500

    design changes

--- resources/templates/partials/tasks-no-button.blade.php
+++ resources/templates/partials/tasks-no-button.blade.php
@@ -4,19 +4,24 @@
             <?php $task_id = [];?>
                 @foreach($row as $task)
                     <div class="col-md-4">
-                        <a href="{!! URL::route('task.detail', $attributes = ['project' => $task->project->slug, 'task' => $task->slug]) !!}">
-                        <div class="panel panel-default">
-                          <div class="panel-heading">
-                            <h3 class="panel-title">{{$task->title}}.</h3>
-                          </div>
-                          <div class="panel-body">
-                            <p>{{$task->description}}</p>
-                            <p><strong>Project: </strong>{{$task->project->title}}</p>
-                            <p class=""><strong>Priority: </strong>{{$task->priority}}</p>
-                          </div>
-                        </div>
-                        </a>
-                    </div>
+                         <a href="{!! URL::route('task.detail', $attributes = ['project' => $task->project->slug, 'task' => $task->slug]) !!}">
+                             <div class="panel panel-default">
+                               <div class="panel-heading panel-heading-<?php echo $task->priority; ?>">
+                                 <h1 class="panel-title">{{ucwords($task->title)}}.</h1>
+                               </div>
+                               <div class="panel-body">
+                                 <div class="body-panel">
+                                     <div class="left-body-panel">
+                                         <p>{{$task->description}}</p>
+                                     </div>
+                                     <div class="right-body-panel">
+                                         <p><strong>{{$task->project->title}}</strong></p>
+                                     </div>
+                                 </div>
+                               </div>
+                             </div>
+                         </a>
+                     </div>
                 @endforeach
             </div>
         @endforeach
--- resources/templates/project/show.blade.php
+++ resources/templates/project/show.blade.php
@@ -4,26 +4,7 @@
 
 <div id="assignments">
     @if(!empty($tasks->toArray()))
-        @foreach(array_chunk($tasks->all(), 3) as $row)
-            <div class="row">
-                @foreach($row as $task)
-                    <div class="col-md-4">
-                        <div class="panel panel-default">
-                          <div class="panel-heading">
-                            <h3 class="panel-title">{{$task->title}}.</h3>
-                          </div>
-                          <div class="panel-body">
-                            <p>{{$task->description}}</p>
-                            <p><strong>Project: </strong>{{$task->project->title}}</p>
-                            <p class=""><strong>Priority: </strong>{{$task->priority}}</p>
-                            <p class="right"><strong>Stage: </strong>{{$task->stage}}</p>
-                            <a href="{!! URL::route('task.detail', $attributes = ['project' => $task->project->slug, 'task' => $task->slug]) !!}">Details</a>
-                          </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-        @endforeach
+        @include('partials.tasks-no-button')
     @else
        <a class="btn btn-primary" href="{!! URL::route('add.task') !!}">Add First Task</a>
     @endif
