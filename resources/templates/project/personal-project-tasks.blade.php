f154c680ab207ca2e41723f659a9d82297bed696
rodrigo <lol@lol.lol>
2014-12-18 14:11:44 -0500

    another bug fix

--- resources/templates/project/personal-project-tasks.blade.php
+++ resources/templates/project/personal-project-tasks.blade.php
@@ -3,7 +3,7 @@
 @section('adminContent')
 
 <div id="assignments">
-    @if( ! is_null($tasks)))
+    @if( ! is_null($tasks))
         @include('partials.tasks-no-button')
     @else
        <a class="btn btn-primary" href="{!! URL::route('add.task') !!}">Add First Task</a>
